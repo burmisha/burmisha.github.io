181982ee431252a9e5227443d72702c1bc7305a4
burmisha <burmisha@gmail.com>
2013-01-27 03:23:17 +0400

    jQuery mobile

---
+++ js/mobile.js
@@ -0,0 +1,7 @@
+$(function(){
+	$("span.button#set-mobile").click(function(event){
+		$("*").toggleClass('mobile');
+		/*$("div#wrapper").toggleClass('mobile');
+		$("span.button").toggleClass('mobile');*/
+	});
+})
