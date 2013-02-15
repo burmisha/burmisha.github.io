e93bdd0e3050d4755d73a43f1e32d9b18b371042
burmisha <burmisha@gmail.com>
2013-02-15 21:04:49 +0400

    add ajax to menu

--- js/mobile.js
+++ js/mobile.js
@@ -1,5 +1,16 @@
+/*
 $(function(){
 	$("span.button#set-mobile").click(function(event){
 		$("*").toggleClass('mobile');
+	});
+})
+*/
+
+$(function(){
+	$("#links").click(function(event){
+		$("#content").load("links.html");
+	});
+	$("#bio").click(function(event){
+		$("#content").load("bio.html");
 	});
 })
