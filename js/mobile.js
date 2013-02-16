44466cb3dbe6ca3fd88ba3c807477e6302e501f4
burmisha <burmisha@gmail.com>
2013-02-17 02:38:39 +0400

    basic photogallery

--- js/mobile.js
+++ js/mobile.js
@@ -7,10 +7,15 @@ $(function(){
 */
 
 $(function(){
+	prefix="http://examplebucket.s3-website-us-east-1.amazonaws.com/"
 	$("#links").click(function(event){
 		$("#content").load("links.html");
 	});
 	$("#bio").click(function(event){
 		$("#content").load("bio.html");
+	});
+	$("#photo").click(function(event){
+		$("#content").load("photo.html");
+		//$("#20130212_Diana div")
 	});
 })
