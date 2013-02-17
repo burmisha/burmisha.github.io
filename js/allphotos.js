05900c66f7d1577df87f7923ed5f1cc31f108d2b
burmisha <burmisha@gmail.com>
2013-02-17 17:35:55 +0400

    bad second gallery

--- js/allphotos.js
+++ js/allphotos.js
@@ -6,3 +6,14 @@ var Diana_21 = {
 	middles: ['0056','0057','0059','0062','0063','0064','0065','0068','0069','0071'],
 	id: '#Diana_20130212',
 };
+
+var Ira_22 = {
+	path: 'photo/20120429_Ira/',
+	path_small: 'photo/20120429_Ira_small/',
+	prefix: '1204-burmisha-',
+	suffix: '.jpg',
+	middles: ['8016','8042','8047','8177','8210'],
+	id: '#Ira_20130212',
+}
+
+allPhotos = [Diana_21, Ira_22];
--- js/mobile.js
+++ js/mobile.js
@@ -9,30 +9,35 @@ $(function(){
 
 	$("#photo").click(function(){
 		$("#content").load("photo.html", function(){
-			var h = Diana_21;
-			for (var i = 0; i < h.middles.length; i++) {
-				var url = h.path_small + h.prefix + h.middles[i]+h.suffix;
-				$(h.id).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
+			for (var j = 0; j < allPhotos.length; j++) {
+				h = allPhotos[j];
+				for (var i = 0; i < h.middles.length; i++) {
+					var url = h.path_small + h.prefix + h.middles[i]+h.suffix;
+					$(h.id).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
+				};
 			};
 		});
-	});
 
-	var h = Diana_21;
-	$('#content').on('click', ".smallphoto", function(){
-		$(h.id).empty();
-		for (var i = 0; i < h.middles.length; i++) {
-			var url = h.path + h.prefix + h.middles[i]+h.suffix;
-			$(h.id).append('<div class="bigphoto" style="background-image:url('+ url + ')"></div>');
-		};
-		return false;
-	});
+		for (var j = 0; j < allPhotos.length; j++) {
+			var h = allPhotos[j];
+			$('#content').on('click', h.id+" .smallphoto", function(){
+				$(h.id).empty();
+				// alert(h.id);
+				for (var i = 0; i < h.middles.length; i++) {
+					var url = h.path + h.prefix + h.middles[i]+h.suffix;
+					$(h.id).append('<div class="bigphoto" style="background-image:url('+ url + ')"></div>');
+				};
+				return false;
+			});
 
-	$('#content').on('click', ".bigphoto", function(){
-		$(h.id).empty();
-		for (var i = 0; i < h.middles.length; i++) {
-			var url = h.path_small + h.prefix + h.middles[i]+h.suffix;
-			$(h.id).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
+			$('#content').on('click', h.id + " .bigphoto", function(){
+				$(h.id).empty();
+				for (var i = 0; i < h.middles.length; i++) {
+					var url = h.path_small + h.prefix + h.middles[i]+h.suffix;
+					$(h.id).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
+				};
+				return false;
+			});
 		};
-		return false;
 	});
 })
