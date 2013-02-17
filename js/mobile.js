c93905d8978cc0ff3e95091336295187b1def546
burmisha <burmisha@gmail.com>
2013-02-17 20:12:51 +0400

    clear up, fix

--- js/mobile.js
+++ js/mobile.js
@@ -22,50 +22,27 @@ $(function(){
 		for (var j = 0; j < allPhotos.length; j++) {
 			var h = allPhotos[j];
 			var hid = "#" + h.id;
-			//alert(h.id);
 			$("#content").append('<div class="photoset" id='+ h.id + '></div>');
 			for (var i = 0; i < h.middles.length; i++) {
 				var url = h.path_small + h.prefix + h.middles[i] + h.suffix;
 				$(hid).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
 			};
 
-			//$('#content').on('click', "{id: $(this).attr("id")}", function(event){
 			$('#content').on('click', "div.photoset", function(event){
-				var p = findSet($(this).attr("id"));
-				var h = allPhotos[p];
+				var h = allPhotos[findSet($(this).attr("id"))];
 				var hid = "#" + h.id;
 				small = $(hid+" div").hasClass("smallphoto");
 				$(hid).empty();
-				alert(small)
-				if (small) {
-					for (var i = 0; i < h.middles.length; i++) {
-						var url = h.path + h.prefix + h.middles[i] + h.suffix;
-						$(hid).append('<div style="background-image:url('+ url + ')"></div>');
-					};
-					$(hid+" div").addClass("bigphoto");
-					alert('ok');
-				} else {
-					for (var i = 0; i < h.middles.length; i++) {
-						var url = h.path_small + h.prefix + h.middles[i] + h.suffix;
-						$(hid).append('<div style="background-image:url('+ url + ')"></div>');
-					};
-					$(hid+" div").addClass("smallphoto");
-					alert('ok');
-				}
-				event.stopPropagation();
-				return false;
-			});
-
-			/*$(hid).on('click', ".bigphoto", function(){
-				$(hid).empty();
-				//alert('cleared ' + hid);
+				var prefix;
+				if (small) { prefix = h.path; } 
+				else { prefix = h.path_small; };
 				for (var i = 0; i < h.middles.length; i++) {
-					var url = h.path_small + h.prefix + h.middles[i] + h.suffix;
-					$(hid).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
+					$(hid).append('<div style="background-image:url('+ prefix + h.prefix + h.middles[i] + h.suffix + ')"></div>');
 				};
-				return false;
-			});*/
-			hid = "qqu";
+				if (small) { $(hid+" div").addClass("bigphoto"); } 
+				else { $(hid+" div").addClass("smallphoto"); };
+				event.stopImmediatePropagation()
+			});
 		};
 	});
 })
