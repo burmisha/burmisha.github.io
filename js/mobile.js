8c4747a3239d1d6ecf5ff55941c62f56cb23b8d7
burmisha <burmisha@gmail.com>
2013-02-17 19:03:35 +0400

    find bug :-)

--- js/mobile.js
+++ js/mobile.js
@@ -12,6 +12,7 @@ $(function(){
 		for (var j = 0; j < allPhotos.length; j++) {
 			var h = allPhotos[j];
 			var hid = "#" + h.id;
+			alert(h.id);
 			$("#content").append('<div class="photoset" id='+ h.id + '></div>');
 			for (var i = 0; i < h.middles.length; i++) {
 				var url = h.path_small + h.prefix + h.middles[i] + h.suffix;
@@ -20,6 +21,7 @@ $(function(){
 
 			$(hid).on('click', ".smallphoto", function(){
 				$(hid).empty();
+				alert('cleared ' + hid);
 				for (var i = 0; i < h.middles.length; i++) {
 					var url = h.path + h.prefix + h.middles[i] + h.suffix;
 					$(hid).append('<div class="bigphoto" style="background-image:url('+ url + ')"></div>');
@@ -29,12 +31,14 @@ $(function(){
 
 			$(hid).on('click', ".bigphoto", function(){
 				$(hid).empty();
+				alert('cleared ' + hid);
 				for (var i = 0; i < h.middles.length; i++) {
 					var url = h.path_small + h.prefix + h.middles[i] + h.suffix;
 					$(hid).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
 				};
 				return false;
 			});
+			hid = "qqu";
 		};
 	});
 })
