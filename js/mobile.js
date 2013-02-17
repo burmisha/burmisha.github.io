4e17b30de2d4211d95ba6509f65cd0a025227398
burmisha <burmisha@gmail.com>
2013-02-17 15:42:06 +0400

    silly big photos

--- js/mobile.js
+++ js/mobile.js
@@ -1,33 +1,36 @@
 $(function(){
 	var Diana_21 = {
-				path : 'photo/20130212_Diana_small/',
+				path: 'photo/20130212_Diana/',
+				path_small: 'photo/20130212_Diana_small/',
 				prefix: '1302-burmisha-',
 				suffix: '.jpg',
 				middles: ['0056','0057','0059','0062','0063','0064','0065','0068','0069','0071'],
 				id: '#Diana_20130212',
-	}
-	;
-	$("#links").click(function(event){
+	};
+	$("#links").click(function(){
 		$("#content").load("links.html");
 	});
 
-	$("#bio").click(function(event){
+	$("#bio").click(function(){
 		$("#content").load("bio.html");
 	});
 
-	$("#photo").click(function(event){
+	$("#photo").click(function(){
 		$("#content").load("photo.html", function(){
-			h = Diana_21;
+			var h = Diana_21;
 			for (var i = 0; i < h.middles.length; i++) {
-				var url = h.path + h.prefix + h.middles[i]+h.suffix;
-				$('#Diana_20130212').append('<div style="background-image:url('+ url + ')"></div>');
+				var url = h.path_small + h.prefix + h.middles[i]+h.suffix;
+				$(h.id).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
 			};
 		});
 	});
-	/*$(Diana_21.id).click(function(event){
-		for (var i = 0; i < h.middles.length; i++) {
+
+	var h = Diana_21;
+	$('#avatar').click(function(){
+		$(h.id).empty();
+			for (var i = 0; i < h.middles.length; i++) {
 			var url = h.path + h.prefix + h.middles[i]+h.suffix;
-			$(h.id).append('<div style="background-image:url('+ url + ')"></div>');
+			$(h.id).append('<div class="bigphoto" style="background-image:url('+ url + ')"></div>');
 		};
-	});*/
+	});	
 })
