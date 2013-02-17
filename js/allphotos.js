5d9180f157ed7d52422a58d4a1613086471431e1
burmisha <burmisha@gmail.com>
2013-02-17 16:59:43 +0400

    one photoset

---
+++ js/allphotos.js
@@ -0,0 +1,8 @@
+var Diana_21 = {
+	path: 'photo/20130212_Diana/',
+	path_small: 'photo/20130212_Diana_small/',
+	prefix: '1302-burmisha-',
+	suffix: '.jpg',
+	middles: ['0056','0057','0059','0062','0063','0064','0065','0068','0069','0071'],
+	id: '#Diana_20130212',
+};
--- js/mobile.js
+++ js/mobile.js
@@ -1,12 +1,4 @@
 $(function(){
-	var Diana_21 = {
-				path: 'photo/20130212_Diana/',
-				path_small: 'photo/20130212_Diana_small/',
-				prefix: '1302-burmisha-',
-				suffix: '.jpg',
-				middles: ['0056','0057','0059','0062','0063','0064','0065','0068','0069','0071'],
-				id: '#Diana_20130212',
-	};
 	$("#links").click(function(){
 		$("#content").load("links.html");
 	});
@@ -26,7 +18,7 @@ $(function(){
 	});
 
 	var h = Diana_21;
-	$('#content').on('click', ".smallphoto",function(){
+	$('#content').on('click', ".smallphoto", function(){
 		$(h.id).empty();
 		for (var i = 0; i < h.middles.length; i++) {
 			var url = h.path + h.prefix + h.middles[i]+h.suffix;
@@ -35,7 +27,7 @@ $(function(){
 		return false;
 	});
 
-	$('#content').on('click', ".bigphoto",function(){
+	$('#content').on('click', ".bigphoto", function(){
 		$(h.id).empty();
 		for (var i = 0; i < h.middles.length; i++) {
 			var url = h.path_small + h.prefix + h.middles[i]+h.suffix;
@@ -43,11 +35,4 @@ $(function(){
 		};
 		return false;
 	});
-
-	/*$('#Diana_20130212 .bigphoto').click(function(){
-		alert("фыфы");
-	});
-	$('#Diana_20130212 .smallphoto').click(function(){
-		alert("dd");
-	});*/
 })
