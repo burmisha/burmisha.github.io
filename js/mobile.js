86908c1e365f8134a76edf50ede36cbf47f797b8
burmisha <burmisha@gmail.com>
2013-02-17 18:57:26 +0400

    clear

--- js/mobile.js
+++ js/mobile.js
@@ -8,36 +8,32 @@ $(function(){
 	});
 
 	$("#photo").click(function(){
-
 		$("#content").empty();
 		for (var j = 0; j < allPhotos.length; j++) {
-			h = allPhotos[j];
+			var h = allPhotos[j];
+			var hid = "#" + h.id;
 			$("#content").append('<div class="photoset" id='+ h.id + '></div>');
 			for (var i = 0; i < h.middles.length; i++) {
-				var url = h.path_small + h.prefix + h.middles[i]+h.suffix;
-				$("#"+h.id).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
+				var url = h.path_small + h.prefix + h.middles[i] + h.suffix;
+				$(hid).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
 			};
-		};
 
-		for (var j = 0; j < allPhotos.length; j++) {
-			var h = allPhotos[j];
-			var hid = "#"+h.id;
-			$('#content').on('click', hid+" .smallphoto", function(){
+			$(hid).on('click', ".smallphoto", function(){
 				$(hid).empty();
 				for (var i = 0; i < h.middles.length; i++) {
-					var url = h.path + h.prefix + h.middles[i]+h.suffix;
+					var url = h.path + h.prefix + h.middles[i] + h.suffix;
 					$(hid).append('<div class="bigphoto" style="background-image:url('+ url + ')"></div>');
 				};
-				//return false;
+				return false;
 			});
 
-			$('#content').on('click', hid + " .bigphoto", function(){
+			$(hid).on('click', ".bigphoto", function(){
 				$(hid).empty();
 				for (var i = 0; i < h.middles.length; i++) {
-					var url = h.path_small + h.prefix + h.middles[i]+h.suffix;
+					var url = h.path_small + h.prefix + h.middles[i] + h.suffix;
 					$(hid).append('<div class="smallphoto" style="background-image:url('+ url + ')"></div>');
 				};
-				//return false;
+				return false;
 			});
 		};
 	});
