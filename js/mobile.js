adf258bf218daf5b4964f4a59fb09e9cf8281cf1
burmisha <burmisha@gmail.com>
2013-02-18 00:41:29 +0400

    add text, 404

--- js/mobile.js
+++ js/mobile.js
@@ -7,12 +7,14 @@ findSet = function(hid) {
 	alert("none for " + hid);
 };
 
-
 addPhotoset = function(h, cl, prefix) {
+	hid = "#" + h.id;
+	$(hid).append('<h2>' + h.title + '</ph2>');
 	for (var i = 0; i < h.middles.length; i++) {
-		$("#" + h.id).append('<div style="background-image:url('+ prefix + h.prefix + h.middles[i] + h.suffix + ')"></div>');
+		$(hid).append('<div style="background-image:url('+ prefix + h.prefix + h.middles[i] + h.suffix + ')"></div>');
 	};
-	$("#" + h.id + " div").addClass(cl);
+	$(hid + " div").addClass(cl);
+	$(hid).append('<p>' + h.text + '</p>');
 }
 
 $(function(){
