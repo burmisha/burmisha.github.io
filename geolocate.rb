d16070e92806ed4182707b2d41a5b205e2613eef
Mikhail Burmistrov <i.like.spam@yandex.ru>
2013-11-14 03:26:43 +0400

    use json

--- geolocate.rb
+++ geolocate.rb
@@ -5,13 +5,12 @@ require 'yaml'
 require 'json'
 
 file = 'db/index.html'
-file = 'db/test.html'
 thing = YAML.load_file(file)
 gps = []
 
 Net::HTTP.start("img-fotki.yandex.ru") do |http|
-  thing['photos'].each_with_index { |file_id, index|
-    path = "/get/" + file_id['url'] + "_orig.jpg"
+  thing['photos'].each_with_index { |image, index|
+    path = "/get/" + image['url'] + "_orig.jpg"
     resp = http.get(path)
     jpeg = StringIO.new
     jpeg << resp.body
@@ -20,12 +19,11 @@ Net::HTTP.start("img-fotki.yandex.ru") do |http|
       latitude = EXIFR::JPEG.new(jpeg).gps.latitude.round(6)
       jpeg.rewind
       longitude = EXIFR::JPEG.new(jpeg).gps.longitude.round(6)
-      thing['photos'].at(index)['gps'] = "#{latitude},#{longitude}"
       puts path + ": " +"#{latitude},#{longitude}"
       gps << {'type' => 'Feature' ,
-              'geometry' => {'type'=> 'Point', 'coordinates' => ["#{latitude}", "#{longitude}"]} ,
+              'geometry' => {'type'=> 'Point', 'coordinates' => ["#{longitude}", "#{latitude}"]} ,
               'properties'=> { 'title' => "Marker #{index}",
-                               'description' => "#{index}",
+                               'image_url' => image['url'],
                                'marker-color' => '#CC0033'} }
     rescue
       puts path + ": " + "\n"
@@ -33,5 +31,4 @@ Net::HTTP.start("img-fotki.yandex.ru") do |http|
   }
 end
 
-File.open(file + '.gps', 'w') {|f| f.write thing.to_yaml }
-File.open(file + '.json', 'w') {|f| f.write gps.to_json }
+File.open(file + '.json', 'w') {|f| f.write 'var geoJson = ' + gps.to_json + ';'}
