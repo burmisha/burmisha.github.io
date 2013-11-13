7756641ff0a3ee42646e260b8a2d7e9e565e590a
Mikhail Burmistrov <i.like.spam@yandex.ru>
2013-11-14 01:40:19 +0400

    fetch all files from web

--- geolocate.rb
+++ geolocate.rb
@@ -1,30 +1,31 @@
 #!/Users/burmisha/.rvm/rubies/ruby-2.0.0-p247/bin/ruby
 require 'exifr'
 require 'net/http'
-require 'stringio'
-
-file_id = '9480/82500796.17/0_cbadd_d8dd2315'
-Net::HTTP.start("img-fotki.yandex.ru") do |http|
-    path = "/get/" + file_id + "_orig.jpg"
-    resp = http.get(path)
-    jpeg = StringIO.new
-    jpeg << resp.body
-    jpeg.rewind
-    latitude = EXIFR::JPEG.new(jpeg).gps.latitude
-    puts latitude
-end
+require 'yaml'
 
 folder="/Users/burmisha/Dropbox/Photo/2013/2013.08 London/DB/"
+thing = YAML.load_file('db/index.html')
 
 File.open('coordinates.tmp', 'w') do |output|
-  Dir.glob(folder + '*.jpg') do |file|
-    coordinates = ""
-    begin 
-      latitude = EXIFR::JPEG.new(file).gps.latitude
-      longitude = EXIFR::JPEG.new(file).gps.longitude
-      output.puts "#{latitude.round(6)},#{longitude.round(6)}"
-    rescue
-      output.puts "\n"
+  Net::HTTP.start("img-fotki.yandex.ru") do |http|
+    #Dir.glob(folder + '*.jpg') do |file|
+    for file_id in thing['photos']
+      path = "/get/" + file_id['url'] + "_orig.jpg"
+      resp = http.get(path)
+      jpeg = StringIO.new
+      jpeg << resp.body
+      begin
+        jpeg.rewind
+        latitude = EXIFR::JPEG.new(jpeg).gps.latitude
+        jpeg.rewind
+        longitude = EXIFR::JPEG.new(jpeg).gps.longitude
+        output.puts "#{latitude.round(6)},#{longitude.round(6)}"
+        puts path + ": " +"#{latitude.round(6)},#{longitude.round(6)}"
+      rescue
+        output.puts "\n"
+        puts path + ": " + "\n"
+      end
     end
   end
 end
+
