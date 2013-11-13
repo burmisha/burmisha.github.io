3ab4b5d3bb5ce3dc58abd99955d191f3973ce44e
Mikhail Burmistrov <i.like.spam@yandex.ru>
2013-11-14 00:52:24 +0400

    add getting gps data from web

--- geolocate.rb
+++ geolocate.rb
@@ -1,5 +1,18 @@
 #!/Users/burmisha/.rvm/rubies/ruby-2.0.0-p247/bin/ruby
 require 'exifr'
+require 'net/http'
+require 'stringio'
+
+file_id = '9480/82500796.17/0_cbadd_d8dd2315'
+Net::HTTP.start("img-fotki.yandex.ru") do |http|
+    path = "/get/" + file_id + "_orig.jpg"
+    resp = http.get(path)
+    jpeg = StringIO.new
+    jpeg << resp.body
+    jpeg.rewind
+    latitude = EXIFR::JPEG.new(jpeg).gps.latitude
+    puts latitude
+end
 
 folder="/Users/burmisha/Dropbox/Photo/2013/2013.08 London/DB/"
 
