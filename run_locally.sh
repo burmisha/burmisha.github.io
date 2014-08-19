b649f95f9be92ac0048bd41adb7f48e2da8521c6
Mikhail Burmistrov <burmisha@gmail.com>
2014-08-19 17:44:18 +0400

    set up enviroment for working on Windows

--- run_locally.sh
+++ run_locally.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -x 
+set -xue -o pipefail -o posix
+export PATH=$PATH:/c/Users/burmisha/AppData/Roaming/npm
 # ./form_geojson.py 
 # cp uk.geojson ~/Dropbox/Photo/2013/2013.08\ London/
 lessc css/colortheme.less.css css/colortheme.css
