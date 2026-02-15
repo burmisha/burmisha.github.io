3984c5cbf89662c72ac64c9748f6d8eea923d9ad
Mikhail Burmistrov <moburmistrov@avito.ru>
2026-02-15 23:23:05 +0400

    Del lesscss

--- run_locally.sh
+++ run_locally.sh
@@ -3,6 +3,7 @@ set -xue -o pipefail -o posix
 export PATH=$PATH:/c/Users/burmisha/AppData/Roaming/npm
 # ./form_geojson.py 
 # cp uk.geojson ~/Dropbox/Photo/2013/2013.08\ London/
-lessc css/colortheme.less.css css/colortheme.css
+# brew install ruby@3.3
+bundle install
 bundle exec jekyll serve
 
